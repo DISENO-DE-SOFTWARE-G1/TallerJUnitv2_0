189ead4b96979c0419da45ae19ab0361330e9894
aszurita <nzuritag@fiec.espol.edu.ec>
2024-08-14 09:40:04 -0500

    codigo base test

--- TallerJUnit/src/test/java/NewEmptyJUnitTest.java
+++ TallerJUnit/src/test/java/NewEmptyJUnitTest.java
@@ -1,3 +1,7 @@
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
@@ -9,6 +13,8 @@
  */
 public class NewEmptyJUnitTest {
     
-    public NewEmptyJUnitTest() {
+    @Test
+    public void NewEmptyJUnitTest() {
+        assertTrue(false);
     }
 }
